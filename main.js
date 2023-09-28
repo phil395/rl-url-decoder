e18bd3039d0b39e93536eb6a6cc1a982b0b84889
phil395 <philiplgvn@gmail.com>
2023-09-28 19:54:44 +0300

    add main script

---
+++ main.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("form.url-form")
+const textarea = form.querySelector("textarea")
+const output = form.querySelector("output")
+
+const EXTRA_PARAMS = ['page', 'sort']
+
+const deleteExtraParams = (url) => {
+  for (const p of EXTRA_PARAMS) url.searchParams.delete(p)
+}
+
+const writeToClipboard = (text) => {
+  navigator.clipboard?.write(text).catch(console.log)
+}
+
+const writeToOutput = (text, className) => {
+  output.textContent = text
+  output.classList.add(className)
+}
+
+const submitHandler = (e) => {
+  e.preventDefault()
+  try {
+    const url = new URL(textarea.value)
+    deleteExtraParams(url)
+    const serializedUrl = decodeURI(url.toString())
+    writeToClipboard(serializedUrl)
+    writeToOutput(serializedUrl, "success")
+  } catch (e) {
+    console.log(e)
+    writeToOutput("Некорректный url адрес", "error")
+  }
+
+}
+
+form.addEventListener("submit", submitHandler)
